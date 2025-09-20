bcd70baed75d1ebcba824d40f3a32016afcb1d95
Angelique Anne Valdez <iamxngelique@gmail.com>
2025-09-21 03:36:09 +0800

    working running old

--- lib/config/constants.dart
+++ lib/config/constants.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart'; 
-
-var host = dotenv.env['HOST'] ?? 'https://jsonplaceholder.typicode.com';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 const Color PRIMARY = Color(0xFF36261C);
 const Color WHITE = Color(0xFFFFFFFF);
+
+var host = dotenv.env['HOST'];
